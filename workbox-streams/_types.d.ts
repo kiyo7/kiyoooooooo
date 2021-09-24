7eb45a0c7cc19c2778781304b1592182247a5197
kiyo7 <kiyo324783@icloud.com>
2021-09-24 23:01:06 +0900

    FIX

--- workbox-streams/_types.d.ts
+++
@@ -1,6 +0,0 @@
-import 'workbox-streams/_version';
-export declare type StreamSource = Response | ReadableStream | BodyInit;
-/**
- * @typedef {Response|ReadableStream|BodyInit} StreamSource
- * @memberof module:workbox-streams
- */
